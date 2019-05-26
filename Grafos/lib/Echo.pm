b41581cfb9b8f266381726116fe84403078e8302
Enmanuel Leon <enma2310@outlook.com>
2019-05-25 22:25:20 -0400

    button to add load nodes and their function

--- Grafos/lib/Echo.pm
+++ Grafos/lib/Echo.pm
@@ -1,10 +1,23 @@
 package Echo;
 use Dancer ':syntax';
 
+use File::Slurp qw(read_file write_file);
+
 our $VERSION = '0.1';
 
 get '/' => sub {
     template 'index';
 };
-
+get '/write_json' =>  sub {
+  my $filename = config->{dwimmer}{json};
+  my $data->{'nodes'}= [
+      params->{nodes}
+  ];
+  
+  write_file ($filename, to_json($data)); #Aqui se imprime el archivo JSON para leer por javascript
+  redirect '/';
+};
+get '/dijkstra' =>  sub {
+  
+};
 true;
--- Grafos/start.bat
+++ Grafos/start.bat
@@ -1,1 +1,2 @@
+start http://localhost:3000
 perl bin/app.pl
